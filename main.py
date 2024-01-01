09d690dd12706f795ac5d76b9bd27a499a1566a6
unknown <josephjung24@gmail.com>
2024-01-01 14:59:28 +0900

    updated the main code to allow selection of the range of files

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from ReadLandsat import TOA_REFLECTANCE
 from Collocate import Collocation
 
 
-def run(input_dir, output_dir1, output_dir2, loc_grid, loc_cloud, region, resolution, ncritical):
+def run(input_dir, output_dir1, output_dir2, loc_grid, loc_cloud, region, resolution, ncritical, count_start, count_end):
     print("GENERATION OF LANSAT SIC | REGION: {} | RESOLUTION: {} | N_CRITICAL: {}".format(region, resolution, ncritical))
     # =============================================================================
     # Read the Polar Stereographic Grid System: The system we want to project to
@@ -36,34 +36,37 @@ def run(input_dir, output_dir1, output_dir2, loc_grid, loc_cloud, region, resolu
     count = 1
     for n in range(len(path_list)):
         for m in range(len(path_list[n])):
-            path = path_list[n][m]
-            filename = os.path.basename(path)
-            cls5 = TOA_REFLECTANCE(5, region, path, loc_cloud)
-            cls6 = TOA_REFLECTANCE(6, region, path, loc_cloud)
-            criteria = cls5.readcloudlabel()
-            _ = cls6.readcloudlabel()
-            if criteria:
+            if count >= count_start and count < count_end:
+                path = path_list[n][m]
+                filename = os.path.basename(path)
+                cls5 = TOA_REFLECTANCE(5, region, path, loc_cloud)
+                cls6 = TOA_REFLECTANCE(6, region, path, loc_cloud)
+                criteria = cls5.readcloudlabel()
+                _ = cls6.readcloudlabel()
+                if criteria:
     # =============================================================================
     # Classifying Ice / Water using NDSI
     # =============================================================================
-                R5 = cls5.ReadLandsat()
-                R6 = cls6.ReadLandsat()
-                lon_l, lat_l = cls5.utm2lonlat()
-                print('Classifying Ice / Water...')
-                NDSI = (R5-R6)/(R5+R6)
-                SI_landsat = np.zeros_like(NDSI)
-                for i in tqdm(range(SI_landsat.shape[0])):
-                    for j in range(SI_landsat.shape[1]):
-                        if (NDSI[i,j] > 0.45) and (R5[i,j] > 0.08):
-                            SI_landsat[i,j] = 1
-                idx_nan = np.where(np.isnan(R5), True, False)
-                SI_landsat[idx_nan] = np.nan
+                    R5 = cls5.ReadLandsat()
+                    R6 = cls6.ReadLandsat()
+                    lon_l, lat_l = cls5.utm2lonlat()
+                    print('Classifying Ice / Water...')
+                    NDSI = (R5-R6)/(R5+R6)
+                    SI_landsat = np.zeros_like(NDSI)
+                    for i in tqdm(range(SI_landsat.shape[0])):
+                        for j in range(SI_landsat.shape[1]):
+                            if (NDSI[i,j] > 0.45) and (R5[i,j] > 0.08):
+                                SI_landsat[i,j] = 1
+                    idx_nan = np.where(np.isnan(R5), True, False)
+                    SI_landsat[idx_nan] = np.nan
     # =============================================================================
     # Calculating Sea Ice Concentration
     # =============================================================================
-                cls_col.collocate(lonpsn, latpsn, lon_l, lat_l, SI_landsat, filename, filename[17:25])
-                print('[{} / {}] GENERATED SIC for {}'.format(count, total_size, filename))
-                count += 1      
+                    cls_col.collocate(lonpsn, latpsn, lon_l, lat_l, SI_landsat, filename, filename[17:25])
+                    print('[{} / {}] GENERATED SIC for {}'.format(count, total_size, filename))
+                else:
+                    print('[{} / {}] REMOVED due to Cloud Contamination {}'.format(count, total_size, filename))
+            count += 1      
     print("GENERATED LANSAT SIC for | REGION: {} | RESOLUTION: {} | N_CRITICAL: {}".format(region, resolution, ncritical))
 
 def parse_opt():
@@ -76,6 +79,8 @@ def parse_opt():
     parser.add_argument('--region', type = str, required = True, help = 'Region Name')
     parser.add_argument('--resolution', type = float, required = True, help = 'Grid Resolution: 6.25, 12.5, 25 km')
     parser.add_argument('--ncritical', type = int, default = 15000, help = 'Critical Sample Size') # 15000 for 6.25 km resolution
+    parser.add_argument('--count_start', type = int, default = 1, help = 'Start Count')
+    parser.add_argument('--count_end', type = int, default = 3000, help = 'End Count')
     opt = parser.parse_args()
     return opt
 
